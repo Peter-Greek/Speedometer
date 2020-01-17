1dfff019da5223f6b193cdca519547b349df5a48
Peter Greek <50223929+Xerxes468893@users.noreply.github.com>
2020-01-16 20:41:49 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,11 +1,11 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 credits "Tameem Imamdad - https://codepen.io/tameemimamdad/pen/vzXVMg"
-ui_page "html/html.html"
+ui_page "html.html"
 files {
-	"html/html.html",
-	"html/script.js",
-	"html/style.css"
+	"html.html",
+	"script.js",
+	"style.css"
 }
 
 client_scripts {
